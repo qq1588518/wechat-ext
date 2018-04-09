99c35beb7696871f3cd3d73a10863763112520ed
Pan Xinyang <seanpan2008@gmail.com>
2018-04-09 20:52:24 +0800

    1

--- src/main/java/io/github/xinyangpan/wechatext/core/WechatExtService.java
+++ src/main/java/io/github/xinyangpan/wechatext/core/WechatExtService.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.Objects;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
 
 import okhttp3.HttpUrl;
 
 public class WechatExtService {
+	private static final Logger log = LoggerFactory.getLogger(WechatExtService.class);
+
 	private WechatExtProperties wechatExtProperties;
 
 	public HttpUrl.Builder commonBuilder() {
@@ -17,10 +21,12 @@ public class WechatExtService {
 	}
 
 	public boolean isSignatureValid(String signature, String token, String timestamp, String nonce) {
+		log.debug("isSignatureValid: signature={}, token={}, timestamp={}, nonce={}", signature, token, timestamp, nonce);
 		String[] strs = new String[] { token, timestamp, nonce };
 		Arrays.sort(strs);
 		String concat = Joiner.on("").join(strs);
 		String calculateSignature = DigestUtils.sha1Hex(concat);
+		log.debug("Calculated Signature: {}", calculateSignature);
 		return Objects.equals(signature, calculateSignature);
 	}
 
