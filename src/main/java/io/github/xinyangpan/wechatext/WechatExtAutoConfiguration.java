e704f2d8e1228fa9b89a20b7bb297c098756882f
Sean Pan <seanpan2008@gmail.com>
2018-04-15 15:31:56 +0800

    1

--- src/main/java/io/github/xinyangpan/wechatext/WechatExtAutoConfiguration.java
+++ src/main/java/io/github/xinyangpan/wechatext/WechatExtAutoConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import io.github.xinyangpan.wechatext.api.CoreApi;
@@ -42,6 +43,12 @@ public class WechatExtAutoConfiguration {
 		RestTemplate restTemplate = new RestTemplate();
 //		restTemplate.setMessageConverters(Lists.newArrayList(new MappingJackson2HttpMessageConverter()));
 		return restTemplate;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 
 	@Bean
